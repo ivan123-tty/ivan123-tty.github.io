b54286bc9cf4cdbe2d4018a2b0889d430776c09b
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-09-17 16:49:40 +0300

    small change in aboutme.html

--- js/aboutme-tr.js
+++ js/aboutme-tr.js
@@ -7,7 +7,7 @@ function langRU() {
 	document.getElementById("main1").innerHTML = "Привет!"
 	document.getElementById("main2").innerHTML = "Я ivan123,<br> подросток интересующийся технологиями.<br> Живу в России."
 	document.getElementById("main5").innerHTML = "Моя электронная почта:<br>ivan123another@gmail.com"
-	document.getElementById("main6").innerHTML = "Дополнительные аккаунты"
+	document.getElementById("main6").innerHTML = "Дополнительные (игровые) аккаунты"
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "svg/uk.svg"
 	document.getElementById("lang").onclick = langEN;
@@ -17,7 +17,7 @@ function langEN() {
 	document.getElementById("main1").innerHTML = "Hello!"
 	document.getElementById("main2").innerHTML = "I'm ivan123,<br> a teenager interested in tech.<br> Living in Russia."
 	document.getElementById("main5").innerHTML = "My e-mail:<br>ivan123another@gmail.com"
-	document.getElementById("main6").innerHTML = "Additional accounts"
+	document.getElementById("main6").innerHTML = "Additional (gaming) accounts"
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "svg/ru.svg"
 	document.getElementById("lang").onclick = langRU;
